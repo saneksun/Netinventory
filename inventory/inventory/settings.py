c70da7e790957ad2b86fb51ebaff559f62bd8259
Alexander <39739673+saneksun@users.noreply.github.com>
2024-03-07 18:40:14 -0500

    Update settings.py

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netinv',
         'USER': 'netinv',
-        'PASSWORD': '12qwaszx',
-        'HOST': '10.128.32.112',
+        'PASSWORD': 'USER_PSWD',
+        'HOST': 'DB_host_IP',
         'PORT': '5432',
     }
 }
